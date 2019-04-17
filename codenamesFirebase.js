bd1dd7607800a2a5e88b966b92b79579686b0755
Kate Park <rudhi1009@gmail.com>
2019-04-17 12:05:36 -0700

    oop-player

---
+++ codenamesFirebase.js
@@ -0,0 +1,40 @@
+class CodenamesFBObject{
+    constructor(gameName, changeCallbackFunction){
+        this.boardName = gameName;
+        this.db = null;
+        this.callback = changeCallbackFunction;
+        this.lastSend = null;
+        
+        this.start = this.start.bind( this );
+        this.initialize();
+    }
+    initialize(){
+        this.load();
+    }
+    load(){
+        $.getScript('https://www.gstatic.com/firebasejs/3.6.8/firebase.js',this.start);
+    }
+    start(){
+          this.db=firebase;
+        this.db.initializeApp(firebaseConfig);
+        this.registerListener();
+    }
+    saveState(newState){
+        this.lastSend = JSON.stringify(newState);
+        this.db.database().ref(this.boardName).set(newState);
+    }
+    getAllData( dataCallback ){
+        this.db.database().ref(this.boardName).once('value', (snapShot)=>{
+            dataCallback( snapshot.val() );
+        });
+    }
+    registerListener(){
+        this.db.database().ref(this.boardName).on('value',this.handleDataUpdate.bind(this));
+    }
+    handleDataUpdate(data){
+        var currentData = JSON.stringify(data.val());
+        if(currentData!=this.lastSend){
+            this.callback.call(null,data.val());
+        }
+    }
+}
---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+            apiKey: "AIzaSyBf6h2GLec6WsULcGgaHy2-uMPEe9L3DGQ",
+            authDomain: "c117connect4.firebaseapp.com",
+            databaseURL: "https://c117connect4.firebaseio.com",
+            projectId: "c117connect4",
+            storageBucket: "c117connect4.appspot.com",
+            messagingSenderId: "1046895016915"
+          };
