12809ca76cd4a727b49140b7cd1f686335e6cb80
Kate Park <rudhi1009@gmail.com>
2019-04-17 17:18:39 -0700

    click handler for submit

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ $(document).ready(initializeApp);
 
 var newGame;
 var generateBoard;
+var player1;
+var player2;
 
 function initializeApp() {
     $('.cover').hide();
+    player1 = new Player(1);
+    player2 = new Player(2);
+     //add click handler to landing page 'start' button
     newGame = new Codenames();
     generateBoard = new Card();
     generateBoard.constructCard();
-    
+    $('.submitbtn').click(player1.getInputVal);
 }
 
 class Player {
@@ -19,8 +24,18 @@ class Player {
         this.playerKeycard = [];
         var newKeycards = new Keycards();
         this.playerKeycard.push(newKeycards.getKeycards());
+        this.clue;
+        this.number;
     }
+    getInputVal() {
+        this.clue = $('.clueinput').val();
+        this.number = $('.numberinput').val();
+        console.log('this.clue', this.clue);
+        console.log('this.number', this.number);
+    }
+
 }
+
 
 class Keycards {
     constructor( ) { 
@@ -36,7 +51,7 @@ class Keycards {
     }
 }
 
-var player1 = new Player(1);
-var player2 = new Player(2);
+// player1 = new Player(1);
+// player2 = new Player(2);
 console.log('player1', player1);
 console.log('player2', player2);
