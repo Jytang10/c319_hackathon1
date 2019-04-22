851f687991ad11b9040fe15849b3fba8ca1c0b0c
James Tang <jytang10@gmail.com>
2019-04-22 09:53:34 -0700

    Final changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,3 @@
-// var firebaseConfig = {
-//             apiKey: "AIzaSyBf6h2GLec6WsULcGgaHy2-uMPEe9L3DGQ",
-//             authDomain: "c117connect4.firebaseapp.com",
-//             databaseURL: "https://c117connect4.firebaseio.com",
-//             projectId: "c117connect4",
-//             storageBucket: "c117connect4.appspot.com",
-//             messagingSenderId: "1046895016915"
-//           };
-
 var firebaseConfig = {
   apiKey: "AIzaSyD-g3R7y3415zZUeoDSzdLanRiYUuiSQUI",
   authDomain: "codenamesduet-67da1.firebaseapp.com",
@@ -15,12 +6,3 @@ var firebaseConfig = {
   storageBucket: "",
   messagingSenderId: "1016360363634"
 };
-
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCpoNH6M6NPGIm5X-cYotdTHCrdes-HMvc",
-//   authDomain: "codenamesduet-67da1.firebaseapp.com",
-//   databaseURL: "https://codenamesduet-67da1.firebaseio.com",
-//   projectId: "ccodenames-duet-testing",
-//   storageBucket: "",
-//   messagingSenderId: "1016360363634"
-// };
